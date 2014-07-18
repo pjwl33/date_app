331dbc8fbdd75ad5d53eb6de028c0188f47b742f
pjwl33 <paul.leejw@gmail.com>
2014-07-18 14:02:12 -0400

    user facebook signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #checking for user signed in
+  def signed_in?
+    true if session[:user_id]
+  end
+
+  #current user object return
+  def current_user
+    if signed_in?
+      User.find(session[:user_id])
+    else
+      redirect_to root_path
+    end
+  end
 end
