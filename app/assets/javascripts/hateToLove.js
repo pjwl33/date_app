fc298974853b8b76344887568ee397d2875ad40c
pjwl33 <paul.leejw@gmail.com>
2014-09-06 23:38:16 -0400

    jquery ui ftw

---
+++ app/assets/javascripts/hateToLove.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  makeDraggable();
+  createSideSwipe();
+});
+
+function makeDraggable(){
+  var matches = $('.find-matches-wrap');
+  for (var i = 0; i < matches.length; i++){
+    matches.eq(i).draggable({
+      drag: function(event, ui){
+        console.log($(this)[0].id);
+      },
+      revert: function(event, ui) {
+        $(this).data("uiDraggable").originalPosition = {
+          top : 0,
+          left : 0
+        };
+        return !event;
+      }
+    });
+  }
+}
+
+function createSideSwipe(){
+  $('#hate-div').droppable({
+    drop: function(event, ui){
+      console.log($(this));
+    }
+  });
+  $('#love-div').droppable({
+    drop: function(event, ui){
+      console.log($(this));
+      makeMatch();
+    }
+  });
+}
+
+function makeMatch(){
+
+}
--- app/assets/javascripts/hate_to_love.js
+++
@@ -1,1 +0,0 @@
-$('.find-matches-wrap').draggable();
