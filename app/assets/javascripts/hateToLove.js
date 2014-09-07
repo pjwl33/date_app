8b098259ba71eb06e64d786bf7056a06900ed882
pjwl33 <paul.leejw@gmail.com>
2014-09-07 02:24:34 -0400

    some stlying

--- app/assets/javascripts/hateToLove.js
+++ app/assets/javascripts/hateToLove.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   makeDraggable();
   createSideSwipe();
+  var matches = $('.find-matches-wrap');
+  matches.eq(0).css('visibility', 'visible');
 });
 
 function makeDraggable(){
